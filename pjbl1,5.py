fa25586c4b3a7ed266db06fcf47e63be403383bf
Rian Septiawan <ryanseptiawan@student.unp.ac.id>
2026-04-08 22:29:22 +0700

    1,5

---
+++ pjbl1,5.py
@@ -0,0 +1,6 @@
+# Membuat file di penyimpanan lokal VM Colab
+with open("data_rahasia.txt", "w") as f:
+    f.write("Ini adalah data penting yang tidak boleh hilang!")
+
+print("File 'data_rahasia.txt' berhasil dibuat di Local VM.")
+
